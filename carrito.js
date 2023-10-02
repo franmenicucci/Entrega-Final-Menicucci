5c0923fac810a4c31706bfb2446b863db3fff835
Franco Menicucci <menicuccifranco23@gmail.com>
2023-10-02 00:23:42 -0300

    Pre-Entrega3-Menicucci

---
+++ carrito.js
@@ -0,0 +1,20 @@
+//   const productosEnElCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"))
+
+//  const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
+//  const contenedorCarritoProductos = document.querySelector("#carrito-productos")
+//   const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
+//      const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
+
+
+//    if (productosEnElCarrito){
+
+//      contenedorCarritoVacio.classList.add("disabled")
+//      contenedorCarritoProductos.classList.remove("disabled")
+//      contenedorCarritoComprado.classList.add("disabled")
+
+//  } else {
+
+//   }
+
+
+
